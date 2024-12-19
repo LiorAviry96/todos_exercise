61e75d4aaaaa80c33d42448ad03fb2d16bb10296
liorav <liorav@wix.com>
2024-12-19 19:12:21 +0200

    change all css

--- pages/TodoDetails.jsx
+++ pages/TodoDetails.jsx
@@ -1,29 +1,31 @@
 import { todoService } from "../services/todo.service.js"
 import { showErrorMsg } from "../services/event-bus.service.js"
-import { loadTodo } from "../store/actions/todo.actions.js"
+//import { loadTodo } from "../store/actions/todo.actions.js"
 const { useState, useEffect } = React
 const { useParams, useNavigate, Link } = ReactRouterDOM
 
 export function TodoDetails() {
 
     const [todo, setTodo] = useState(null)
-    const params = useParams()
+    //const params = useParams()
     const navigate = useNavigate()
+    const { todoId } = useParams()
+
 
     useEffect(() => {
-        loadTodo()
-    }, [params.todoId])
+        if (todoId) loadTodo()
+    }, [todoId])
 
 
-    /*function loadTodo() {
-        todoService.get(params.todoId)
+    function loadTodo() {
+        todoService.get(todoId)
             .then(setTodo)
             .catch(err => {
                 console.error('err:', err)
                 showErrorMsg('Cannot load todo')
                 navigate('/todo')
             })
-    }*/
+    }
 
     function onBack() {
         // If nothing to do here, better use a Link
@@ -37,12 +39,12 @@ export function TodoDetails() {
             <h1 className={(todo.isDone)? 'done' : ''}>{todo.txt}</h1>
             <h2>{(todo.isDone)? 'Done!' : 'In your list'}</h2>
 
-            <h1>Todo importance: {todo.importance}</h1>
+            <h3>Todo importance: {todo.importance}</h3>
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim rem accusantium, itaque ut voluptates quo? Vitae animi maiores nisi, assumenda molestias odit provident quaerat accusamus, reprehenderit impedit, possimus est ad?</p>
             <button onClick={onBack}>Back to list</button>
             <div>
-                <Link to={`/todo/${todo.nextTodoId}`}>Next Todo</Link> |
-                <Link to={`/todo/${todo.prevTodoId}`}>Previous Todo</Link>
+                <Link to={`/todo/${todo.nextTodoId}`}><u>Next Todo</u></Link> |
+                <Link to={`/todo/${todo.prevTodoId}`}><u>Previous Todo</u></Link>
             </div>
         </section>
     )
